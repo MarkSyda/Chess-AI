c7e597e85672b7e4f6b1abc1355048fb0c0ad01a
Mark <sudamark11@gmail.com>
2025-06-18 13:28:46 +0300

    Pawn logic complete

--- Chess AI/src/main.cpp
+++ Chess AI/src/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+ï»¿#include <iostream>
 #include <array>
 #include <vector>
 #include <bitset>
@@ -13,13 +13,18 @@ struct BitBoards {
 	const U64 StartBitBoard = 0b1111111111111111000000000000000000000000000000001111111111111111;
 	const U64 WPawnStartBitBoard = 0b0000000000000000000000000000000000000000000000001111111100000000;
 	const U64 BPawnStartBitBoard = 0b0000000011111111000000000000000000000000000000000000000000000000;
-	U64 PieceBitBoard = 0b1111111111111111000000000000000000000000000000001111111111111111;
-	U64 OccBitBoard = 0b1111111111111111000000000000000000000000000000011111111111111111;
+
+	U64 PieceBitBoard = 0b1111111111111111000000000000000000000000000000011111111111111111;
+
 	U64 BlackPiecesBitBoard = 0b1111111111111111000000000000000000000000000000000000000000000000;
-	U64 WhitePiecesBitBoard = 0b0000000000000000000000000000000000000000000000001111111111111111;
-	U64 WhitePawnBitBoard = 0b0000000000000000000000000000000000000000000000001111111100000000;
+	U64 WhitePiecesBitBoard = 0b0000000000000000000000000000000000000000000000011111111111111111;
+
+	U64 WhitePawnBitBoard = 0b0000000000000000000000000000000000000000000000011111111100000000;
 	U64 BlackPawnBitBoard = 0b0000000011111111000000000000000000000000000000000000000000000000;
-	U64 IsPawnOnStartSq = WhitePawnBitBoard & StartBitBoard;
+
+	U64 WhiteKnightBitBoard = 0b0000000000000000000000000000000000000000000000000000000001000010;
+	U64 BlackKnightBitBoard = 0b0100001000000000000000000000000000000000000000000000000000000000;
+	
 };
 
 class makeMove {
@@ -33,72 +38,149 @@ public:
 			return 1;
 		}
 	}
-	void PawnLogic() {
+	//TODO: asenda koik vectorid arraydega U64 arraydega
+	// int Color = 0 - White, 1->Black
+	void PawnLogic(int Color) {
 		BitBoards BB;
 		BB.StartBitBoard;
-		U64 StartSqPawns = BB.IsPawnOnStartSq;
-		U64 OccBitBoard = BB.OccBitBoard;
+		U64 OccBitBoard = BB.BlackPiecesBitBoard | BB.WhitePiecesBitBoard;
 		U64 PBitBoard = BB.PieceBitBoard;
-		std::vector<int64_t> LegalMoves;
-
-		//init vector NotMovedPawns
-		std::vector<int>NotMovedPawns;
-		TurnedBitLoc(StartSqPawns, NotMovedPawns); //Updateme oma vectorit ja paneme sinna NotMovedPawnide id-d
-
-		//Init vec Alivepawns
+		U64 StartSqPawns = 0;
+		std::vector<U64> LegalMoves;
 		std::vector<int> AlivePawns;
-		TurnedBitLoc(BB.WhitePawnBitBoard, AlivePawns);
+		std::vector<int> NotMovedPawns;
+		uint8_t count = 0;
+
+		//Determine color
+		if (Color == 0) {
+			TurnedBitLoc(BB.WhitePawnBitBoard, AlivePawns);
+			StartSqPawns = BB.WhitePawnBitBoard & BB.StartBitBoard;
+			TurnedBitLoc(StartSqPawns, NotMovedPawns);
+			count = countSetBits(StartSqPawns);
+		}
+
+		else if (Color == 1) {
+			TurnedBitLoc(BB.BlackPawnBitBoard, AlivePawns);
+			StartSqPawns = BB.BlackPawnBitBoard & BB.StartBitBoard;
+			TurnedBitLoc(StartSqPawns, NotMovedPawns);
+			count = countSetBits(StartSqPawns);
+		}
 
 		//Printing Bitboards
-		PrintBitboard(BB.OccBitBoard, std::string("OccBitBoard"));
+		PrintBitboard(OccBitBoard, std::string("OccBitBoard"));
 		PrintBitboard(BB.PieceBitBoard, std::string("PieceBitBoard"));
 
-		uint8_t count = countSetBits(StartSqPawns); //Loeme mitte liigutatud Pawnid üle 
-
-		for (unsigned int i = 0; i < count; i++) {
-			U64 future_move_1 = BitMap_Change_Vert(PBitBoard, NotMovedPawns[i], 16);
-			if (!IsColliding(OccBitBoard, NotMovedPawns[i], 16) && !IsBlocked(OccBitBoard, 1, NotMovedPawns[i], 16)) {
-				LegalMoves.push_back(future_move_1);
-				size_t vec_size = LegalMoves.size();
-				std::cout << "Amount of legal moves (for loop 2): " << vec_size << "\n";
-			}
-		} std::cout << "\n";
-
+		//Pawns
+		
+		//Single move (1 sq)
 		for (unsigned int i = 0; i < AlivePawns.size(); i++) {
-			U64 future_move = BitMap_Change_Vert(PBitBoard, AlivePawns[i], 8);
-			if (!IsColliding(OccBitBoard, AlivePawns[i], 8)) {
+			U64 future_move = BitMap_Change_Vert(PBitBoard, AlivePawns[i], 8, Color);
+			if (!(IsColliding(OccBitBoard, AlivePawns[i], 8, Color))) {
 				LegalMoves.push_back(future_move);
-				size_t vec_size = LegalMoves.size();
-				std::cout << "Amount of legal moves (for loop 1): " << vec_size << "\n";
 			}
 		}
 
-		for (unsigned int i = 0; i < AlivePawns.size(); i++) {
-			U64 future_move[2];
-			U64 Capture_able_board = CanCapture(OccBitBoard, 1, AlivePawns[i]);
-			future_move[0] = BitMap_Change_Vert(PBitBoard, AlivePawns[i], 7);
-			future_move[1] = BitMap_Change_Vert(PBitBoard, AlivePawns[i], 9);
+		//Double move (2 sq)
+		for (unsigned int i = 0; i < count; i++) {
+			U64 future_move_1 = BitMap_Change_Vert(PBitBoard, NotMovedPawns[i], 16, Color);
+			if (!IsColliding(OccBitBoard, NotMovedPawns[i], 16, Color) && !IsBlocked(OccBitBoard, 1, NotMovedPawns[i], 16, Color)) {
+				LegalMoves.push_back(future_move_1);
+			}
+		}
 
-			if ((Capture_able_board & future_move[0]) != 0) {
+		//Pawn taking
+		for (unsigned int i = 0; i < AlivePawns.size(); i++) { // Alivepawns[i] naitab pawni bitnummerit
+			U64 future_move[2] = { 0 };
+			U64 Capture_able_board = 0;
+			U64 file = AlivePawns[i] % 8; // File {0-7}
+			//Color selection
+			if (Color == 0) {
+				Capture_able_board = CanCapture(BB.BlackPiecesBitBoard, 1, AlivePawns[i], Color);
+			}
+			else if (Color == 1) {
+				Capture_able_board = CanCapture(BB.WhitePiecesBitBoard, 1, AlivePawns[i], Color);
+			}
+
+			if (OuterFileCheck(AlivePawns[i]) != 0) {// Outer pawnidele eraldi check
+
+				if (file == 0) {
+					future_move[1] = BitMap_Change_Vert(PBitBoard, AlivePawns[i], 9, Color);
+					future_move[1] = Capture_able_board & future_move[1];
+				}
+				if (file == 7) {
+					future_move[0] = BitMap_Change_Vert(PBitBoard, AlivePawns[i], 7, Color);
+					future_move[0] = Capture_able_board & future_move[0];
+				}
+			}
+			if (OuterFileCheck(AlivePawns[i]) == 0) {
+				future_move[0] = BitMap_Change_Vert(PBitBoard, AlivePawns[i], 7, Color);
+				future_move[1] = BitMap_Change_Vert(PBitBoard, AlivePawns[i], 9, Color);
+				future_move[0] = Capture_able_board & future_move[0];
+				future_move[1] = Capture_able_board & future_move[1];
+			}
+			if (future_move[0] != 0) {
 				LegalMoves.push_back(future_move[0]);
-				size_t vec_size = LegalMoves.size();
-				std::cout << "Amount of legal moves (for loop 3): " << vec_size << "\n";
 			}
-			else if ((Capture_able_board & future_move[1]) != 0) {
+			else if (future_move[1] != 0) {
 				LegalMoves.push_back(future_move[1]);
-				int64_t vec_size = LegalMoves.size();
-				std::cout << "Amount of legal moves (for loop 3): " << vec_size << "\n";
-			}std::cout << "\n";
+			}
 		}
+		legal_moves_test(LegalMoves);
 	}
-	void WhiteMove() {
+	
+	void PieceLogic(int Color) {
+		BitBoards bitboard;
+		U64 WhiteKnightBitBoard = bitboard.WhiteKnightBitBoard;
+		U64 BlackKnightBitBoard = bitboard.BlackKnightBitBoard;
+		//Knights
+		std::vector<int>AliveKnights;
+		if (Color == 0) {
+			TurnedBitLoc(WhiteKnightBitBoard, AliveKnights); //push back vector. gives aliveknights bitnumbers
+		}
+		else if (Color == 1) {
+			TurnedBitLoc(BlackKnightBitBoard, AliveKnights);
+		}
 
+		for (unsigned int i = 0; i < AliveKnights.size(); i++) {
+			U64 future_move[8] = {0};
+			future_move[0] <<= 
+
+		}
 	}
 private:
-	void PrintBitboard(int64_t BitBoard, std::string Name) {
+	void PrintBetterBitboard(U64 BitBoard, U64 wBitboard, U64 bBitboard, std::string name) {
+		std::bitset<64> bits(BitBoard);
+
+		std::cout << "BITBOARD_NAME::" << name << "\n";
+
+		for (int row = 0; row < 8; ++row) {
+			for (int column = 0; column < 8; ++column) {
+				int index = (7 - row) * 8 + column;
+				U64 mask = 1ULL << index;
+
+				if (BitBoard & mask) {
+					if (wBitboard & mask) {
+						std::cout << "W ";
+					}
+					else if (bBitboard & mask) {
+						std::cout << "B ";
+					}
+					else {
+						std::cout << "M ";
+					}
+				}
+				else {
+					std::cout << "O ";
+				}
+			}
+			std::cout << "\n";
+		}
+		std::cout << "\n";
+	}
+	void PrintBitboard(U64 BitBoard, std::string Name) {
 		std::bitset<64> bits(BitBoard);  // Create bitset from BitBoard
 
-		std::cout << "BITBOARD_NAME::" << Name << "\n\n";
+		std::cout << "BITBOARD_NAME::" << Name << "\n";
 
 		// Print the bitboard row by row
 		for (int row = 0; row < 8; ++row) {
@@ -109,8 +191,18 @@ private:
 			}
 			std::cout << "\n";
 		}
+		std::cout << "\n";
 	}
-	
+
+	void legal_moves_test(std::vector<U64>LegalMoves) {
+		BitBoards bitboard;
+		std::cout << "Amount of legal moves available:" << LegalMoves.size() << "\n";
+		int n = LegalMoves.size();
+		for (int i = 0; i < n; i++) {
+			PrintBetterBitboard(LegalMoves[i],bitboard.WhitePiecesBitBoard, bitboard.BlackPiecesBitBoard, "Legal move nr" + std::to_string(i+1));
+		}
+	}
+
 	void TurnedBitLoc(U64 number, std::vector<int>& turnedOnBits) {
 		// Loop through all 64 bits
 		for (int bitIndex = 0; bitIndex < 64; bitIndex++) {
@@ -121,20 +213,21 @@ private:
 		}
 	}
 
+	int SingleTurnedBitLoc(U64 N) {
+		U64 result = log2(N);
+		return result;
+	}
+
 	void RemovePiece(U64 BitMap, int BitNumber) {
 		FlipBit(BitMap, BitNumber);
 	}
 
-	bool OuterFileCheck(int Bitnumber) {
+	U64 OuterFileCheck(int Bitnumber) {
 		U64 OuterFile = 0b1111111110000001100000011000000110000001100000011000000111111111;
 		U64 tempmap = CreateTempBit(Bitnumber);
+		//PrintBitboard(OuterFile, "outerfile");
 		tempmap = tempmap & OuterFile;
-		if (tempmap != 0) {
-			return true;
-		}
-		else {
-			return false;
-		}
+		return tempmap;
 	}
 
 	int RecognizeFile(std::string SquareAdress) {
@@ -157,7 +250,7 @@ private:
 		std::cout << BitNumber << "\n";
 		return BitNumber; 
 	}
-
+	
 	//Brian Kernighan's way BigCounting
 	U8 countSetBits(U64 n) {
 		U8 count = 0;
@@ -178,60 +271,105 @@ private:
 		return N ^= (1ULL << (BitPos));
 	}
 
-	U64 BitMap_Change_Vert(U64 BitMap, int BitNumber, int BitShiftAmount) {
+	U64 BitMap_Change_Vert(U64 BitMap, int BitNumber, int BitShiftAmount, int Dir) {
 		U64 TempBitMap_1 = CreateTempBit(BitNumber);
 		U64 TempBitMap_2 = CreateTempBit(BitNumber);
-		TempBitMap_2 = TempBitMap_1 << BitShiftAmount;
+		if (Dir == 0) {
+			TempBitMap_2 = TempBitMap_1 << BitShiftAmount;
+		}
+		else if (Dir == 1) {
+			TempBitMap_2 = TempBitMap_1 >> BitShiftAmount;
+		}
 		TempBitMap_2 = TempBitMap_1 | TempBitMap_2;
 		U64 result = BitMap^ TempBitMap_2;
 		//PrintBitboard(result, "Result");
 		return result;
 	}
 
-	// int Piece 1 = pawn, 2 = knigt, 3 = bishop, 5 = rook, 9 = queen
-	U64 CanCapture(U64 BitMap, int Piece, int BitNumber) {
+	// int Piece 1 = pawn, 2 = knigt, 3 = bishop, 5 = rook, 9 = queen, // Dir -> Color
+	U64 CanCapture(U64 BitMap, int Piece, int BitNumber, int Dir) {
 		U64 TempBitMap_1 = CreateTempBit(BitNumber);
 		U64 TempBitMap_2 = CreateTempBit(NULL);
 
 		if (Piece == 1) {
+			//Special logic for outer files
 			if (OuterFileCheck(BitNumber) && BitNumber % 2 == 0) {
-				TempBitMap_1 = TempBitMap_1 << 9;
+				if (Dir == 0){
+					TempBitMap_1 = TempBitMap_1 << 9; 
+				}
+				else if (Dir == 1) {
+					TempBitMap_1 = TempBitMap_1 >> 7;
+				}
 				TempBitMap_1 = TempBitMap_1 & BitMap;
 				return TempBitMap_1;
 			}
+			//Special logic for outer files
 			else if (OuterFileCheck(BitNumber) && BitNumber % 2 == 1) {
-				TempBitMap_1 = TempBitMap_1 << 7;
+				if (Dir == 0) {
+					TempBitMap_1 = TempBitMap_1 << 7; 
+				}
+				else if (Dir == 1) {
+					TempBitMap_1 = TempBitMap_1 >> 9; 
+				}
 				TempBitMap_1 = TempBitMap_1 & BitMap;
 				return TempBitMap_1;
 			}
 			else {
-				TempBitMap_1 = TempBitMap_1 << 7;
-				TempBitMap_2 = TempBitMap_1 << 2;
+				if (Dir == 0) {
+					TempBitMap_1 = TempBitMap_1 << 7;
+					TempBitMap_2 = TempBitMap_1 << 2;
+				}
+				else if (Dir == 1) {
+					TempBitMap_1 = TempBitMap_1 >> 7;
+					TempBitMap_2 = TempBitMap_1 >> 2;
+				}
+				
+
 				TempBitMap_2 = TempBitMap_1 | TempBitMap_2;
 				TempBitMap_1 = TempBitMap_1 & BitMap;
 				return TempBitMap_1;
 			}
+		}
+
+		if (Piece == 2) {
+
 		}
 		else {
 			std::cout << "\n\n" << "SOMETHING::HAS::GONE::WRON::LEGAL::MOVE::CAPTURE::ERROR" << "\n\n";
 			return 0;
 		}
 	}
-	bool IsColliding(U64 BitMapCollision, int BitNumber, int BitShiftAmount) {
+	bool IsColliding(U64 BitMapCollision, int BitNumber, int BitShiftAmount, int Dir) {
 		U64 TempBitMap_1 = CreateTempBit(BitNumber);
-		TempBitMap_1 = TempBitMap_1 << BitShiftAmount; // after bitshift = movedpos
+		if (Dir == 0) {
+			TempBitMap_1 = TempBitMap_1 << BitShiftAmount; // after bitshift = movedpos
+		}
+		else if (Dir == 1) {
+			TempBitMap_1 = TempBitMap_1 >> BitShiftAmount; // after bitshift = movedpos
+		}
 		TempBitMap_1 = TempBitMap_1 & BitMapCollision;
 		
 		return !TempBitMap_1 == 0;
 	}
 	// int Piece 1 = pawn, 3 = bishop, 5 = rook, 9 = queen
-	bool IsBlocked(U64 BitMap, int Piece, int BitNumber, int BitShiftAmount) {
+	bool IsBlocked(U64 BitMap, int Piece, int BitNumber, int BitShiftAmount, int Color) {
 		U64 TempBitMap_1 = CreateTempBit(BitNumber);
-		U64 TempBitMap_2 = CreateTempBit(NULL); //NULL teeb tühja bitmapi
-		TempBitMap_2 = TempBitMap_1 << BitShiftAmount; // TempBitMap 2 = new pos
-		
+		U64 TempBitMap_2 = CreateTempBit(NULL); //NULL teeb tÃ¼hja bitmapi
+
+		if (Color == 0) {
+			TempBitMap_2 = TempBitMap_1 << BitShiftAmount;
+		}
+		else if (Color == 1) {
+			TempBitMap_2 = TempBitMap_1 >> BitShiftAmount;
+		}
+
 		if (Piece == 1) { // Expected for 2sq pawn moves
-			TempBitMap_2 = TempBitMap_2 >> 8;
+			if (Color == 0) {
+				TempBitMap_2 = TempBitMap_2 >> 8;
+			}
+			else if (Color == 1) {
+				TempBitMap_2 = TempBitMap_2 << 8;
+			}
 			TempBitMap_2 = TempBitMap_2 & BitMap;
 			return !TempBitMap_2 == 0;
 		}
@@ -242,9 +380,6 @@ private:
 int main() {
 	BitBoards BB;
 	makeMove game;
-	game.PawnLogic();
+	game.PawnLogic(0);
 	return 0;
 }
-
-
-
